09bb3a06c959edcb7d1feb882eab9f3374d55fdf
Mihajatiana-dev <hei.mihajatiana@gmail.com>
2024-06-04 22:58:47 +0300

    add employe action 1 and 3

--- interface.js
+++ interface.js
@@ -18,106 +18,113 @@ pool.connect(function (err, client, done) {
         console.log();
     }
 
-function fonction() {
-    console.log("Quel est votre role dans cette patisserie?");
-    console.log("1 - employe");
-    console.log("2 - client");
-    let role = +prompt("=> ");
-    console.log();
-    return role;
-}
+    function fonction() {
+        console.log("Quel est votre role dans cette patisserie?");
+        console.log("1 - employe");
+        console.log("2 - client");
+        let role = +prompt("=> ");
+        console.log();
+        return role;
+    }
 
-function choix(role) {
-    if (role == 1) {
-        console.log("Que souhaitez-vous faire?");
-        console.log("1 - Voir la liste des gateaux");
-        console.log("2 - Voir le nombre de client dans le mois");
-        console.log("3 - Ajouter un nouveau gateau");
-        console.log("4 - Enlever un gateau");
-        console.log("5 - Quitter");
-        let action_employe = +prompt("=>");
-        
-        if (action_employe == 1) {
-            client.query('select * from gateau', (err, res) => {
-                console.log(res("rows"));
-            });
-        }
-        else if (action_employe == 2) {
-            //voir le nombre de client dans le mois
-        }
-        else if (action_employe == 3) {
-            let id_gateau = +prompt("Insérer id :");
-            let nom_gateau = prompt("Insérer nom :");
-            let description = prompt("Insérer la description :");
-            let categorie = prompt("Insérer la categorie :");
-            let prix = +prompt("Insérer le prix :");
-            let id_employe = +prompt("Insérer l'id de l'employe qui le prepare :");
-            let id_promotion = +prompt("Insérer l'id de la promotion :");
+    function choix(role) {
+        if (role == 1) {
+            console.log("Que souhaitez-vous faire?");
+            console.log("1 - Voir la liste des gateaux");
+            console.log("2 - Voir le nombre de client dans le mois");
+            console.log("3 - Ajouter un nouveau gateau");
+            console.log("4 - Enlever un gateau");
+            console.log("5 - Quitter");
+            let action_employe = +prompt("=>");
 
-            const query = `
-                    insert into gateau (id_gateau, nom_gateau, description, categorie, prix, id_employe, id_promotion)
-                    values ($1, $2, $3, $4, $5, $6, $7)
-                `;
+            if (action_employe == 1) {
+                client.query('SELECT * FROM gateau', (err, res) => {
+                    console.log('Liste des gateaux :');
+                    console.log(res.rows);
+                    done();
+                });
+            }
+
+            else if (action_employe == 2) {
+                //voir le nombre de client dans le mois
+            }
+            else if (action_employe == 3) {
+                let id_gateau = +prompt("Insérer id :");
+                let nom_gateau = prompt("Insérer nom :");
+                let description = prompt("Insérer la description :");
+                let categorie = prompt("Insérer la categorie :");
+                let prix = +prompt("Insérer le prix :");
+                let id_employe = +prompt("Insérer l'id de l'employe qui le prepare :");
+                let id_promotion = +prompt("Insérer l'id de la promotion :");
+
+                const query = `
+                INSERT INTO gateau (id_gateau, nom_gateau, description, categorie, prix, id_employe, id_promotion)
+                VALUES ($1, $2, $3, $4, $5, $6, $7)
+            `;
 
                 client.query(query, [id_gateau, nom_gateau, description, categorie, prix, id_employe, id_promotion], (err, res) => {
                     if (err) {
+                        console.log();
                         console.log("Erreur dans l'insertion");
                         choix(role);
                     } else {
-                        console.log('Insertion reussie');
+                        console.log('Insertion réussie');
+                        done();
                     }
-                    done();
                 });
+            }
+
+            else if (action_employe == 4) {
+                //enlever un gateau
+            }
+            else if (action_employe == 5) {
+                console.log("Merci, a tres bientot");
+                done();
+            }
+            else {
+                console.log("Reessayer");
+                choix(role)
+            }
         }
-        else if (action_employe == 4) {
-            //enlever un gateau
-        }
-        else if (action_employe == 5) {
-            console.log("Merci, a tres bientot");
+        else if (role == 2) {
+            console.log("Que souhaitez-vous faire?");
+            console.log("1 - Voir la liste des gateaux");   //nom, prix, categorie
+            console.log("2 - Faire une commande");
+            console.log("3 - Voir la description d'un gateau");
+            console.log("4 - Quitter");
+            let action_client = prompt("=>");
+
+            if (action_client == 1) {
+                //voir la liste des gateaux
+            }
+            else if (action_client == 2) {
+                //faire une commande
+            }
+            else if (action_client == 3) {
+                //voir la description d'un gateau
+            }
+            else if (action_client == 4) {
+                console.log("Merci d'avoir utilise notre service");
+                done();
+            }
+            else {
+                console.log("Reessayer");
+                choix(role)
+            }
         }
         else {
-            console.log("Reessayer");
-            choix(role)
+            console.log("Qui etes-vous?");
+            result();
         }
     }
-    else if (role == 2) {
-        console.log("Que souhaitez-vous faire?");
-        console.log("1 - Voir la liste des gateaux");   //nom, prix, categorie
-        console.log("2 - Faire une commande");
-        console.log("3 - Voir la description d'un gateau");
-        console.log("4 - Quitter");
-        let action_client = prompt("=>");
-        
-        if (action_client == 1) {
-            //voir la liste des gateaux
-        }
-        else if (action_client == 2) {
-            //faire une commande
-        }
-        else if (action_client == 3) {
-            //voir la description d'un gateau
-        }
-        else if (action_client == 4) {
-            console.log("Merci d'avoir utilise notre service");
-        }
-        else {
-            console.log("Reessayer");
-            choix(role)
-        }
+
+    function result() {
+        let role = fonction();
+        choix(role);
     }
-    else {
-        console.log("Qui etes-vous?");
-        result();
-    }
-}
-
-function result() {
-    let role = fonction();
-    choix(role);
-}
 
 
-//appel des fontions
-welcome();
-result();
+    //appel des fontions
+    welcome();
+    result();
 });
